108ae015a9d675d1442013a7c7f0f4eb591421ae
vivekrqwat <vivekrawaturfsam@gmail.com>
2026-08-30 21:44:53 +0530

    changes in ci/cd

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
-if(dir=="up"){
-        this.anims.play(name+"-walk-up",true)
-        this.setVelocityY(-speed)
-       
-    }else if(dir=="down"){
-          this.setVelocityY(speed)
-            
-             this.anims.play(name+"-walk-front",true)
-
-    }else if(dir=="left"){
-        this.setVelocityX(-speed)
-         this.anims.play(name+"-walk-left",true)
-    }else if(dir=="right"){ 
-        this.setVelocityX(speed)
-        this.anims.play(name+"-walk-right",true)
-    }efmel,f
+//for testing
